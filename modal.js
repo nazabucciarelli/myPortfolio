4a12947e2af7343549d75cce5e1168cbb665c736
Nazareno Bucciarelli <bnazareno03@gmail.com>
2022-08-08 16:13:41 -0300

    Fixing login

--- modal.js
+++ modal.js
@@ -1,11 +1,15 @@
-const btnOpen = document.getElementById("btn-login")
-const btnClose = document.getElementById("btn-login-close")
-const modal = document.getElementById("modal")
+const btnOpen = document.getElementById("btn-login");
+const btnClose = document.getElementById("btn-login-close");
+const modal = document.getElementById("modal");
+const body = document.getElementById("body");
 
 btnOpen.addEventListener("click",()=>{
     modal.showModal();
+    body.classList.add("noscroll");
 })
 
 btnClose.addEventListener("click",()=>{
     modal.close();
+    body.classList.remove("noscroll");
 })
+
