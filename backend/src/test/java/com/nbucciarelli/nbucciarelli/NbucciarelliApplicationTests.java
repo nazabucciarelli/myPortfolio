21027e4088c4889fc9164881b57010a6ad70e40d
Nazareno Bucciarelli <bnazareno03@gmail.com>
2022-10-23 15:36:58 -0300

    Reestructurando repo

---
+++ backend/src/test/java/com/nbucciarelli/nbucciarelli/NbucciarelliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbucciarelli.nbucciarelli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbucciarelliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
